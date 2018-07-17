7c4ba342f3e9e03d420f55c88cb22c5966bea5fd
NormaAngie <norma_angelica@hotmail.com>
2018-07-16 20:08:20 -0500

    first commit

---
+++ ejercicio.py
@@ -0,0 +1,58 @@
+#print("Hola Alumnos")
+#print("Adios mundo cruel")
+#uncomentario
+#ejercicio1
+#a=0
+#for i in range(5):
+ #   a+=1
+  #      print(a)
+
+
+#def mensaje():
+  #  print("Impime1")
+   # print("implem2")
+
+#mensaje()
+#Ejercicio=6
+#def sum(num1,num2):
+ #   resultado=num1+num2
+  #  return resultado
+#almacena_resultado=sum(5,8)
+#print(almacena_resultado)
+
+
+#Ejercicio 11 listas(sert
+#miLista=["Maria","Pepe","Marta","Antonio"]
+#print(miLista[0:2])
+#print(miLista[:2])
+#print(miLista[2:0])
+#miLista.append("Sandra")
+#miLista.insert(2,"Angie")
+#miLista.extend(["Ana","César"])
+#print(miLista.index("César"))
+#print(miLista[:])
+#print("César" in miLista)
+#miLista=["Maria",78.21,True,5]
+#miLista.remove(5)
+#print(miLista[:])
+#miLista.pop() #remueve el úmo elementodela lista
+
+#para concatenar 3 veces la misma lista
+#practica Tuplas
+
+mitupla=("Juan",13,1,1995)
+miLista=list(mitupla)
+print(mitupla[1])
+print(miLista)
+
+miLista=["Juan",13,1,1995,"Juan"]
+mitupla=tuple(miLista)
+print(mitupla)
+print("Juan" in mitupla)
+print(mitupla.count(13))
+print(mitupla.count("Juan"))
+print(len(mitupla))
+
+
+
+
